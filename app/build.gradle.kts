515206ab2241a492a6cdf5928ca61e5c581c7501
Arihant Tripathy <arihanttr@live.com>
2025-07-20 17:12:54 +0530

    Generate build outputs for v1.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.arihant.streaks"
         minSdk = 31
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.2"
+        versionCode = 5
+        versionName = "1.3"
     }
 
     buildTypes {
